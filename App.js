43c113d0e1ee3143b881927e8299b87ec88dabd7
David Gaspar <davidgaspar.dev@gmail.com>
2018-07-11 23:03:56 -0300

    Updated design & added comments

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
+// Library code structure React
 import React, { Component } from 'react';
+
+// Native views to Android and iOS
 import { StyleSheet, View, Text } from 'react-native';
 
 export default class App extends Component {
 
+  // @constructor App
+  // @param Property
   constructor(props) {
     super(props);
 
+    // State initialization
     this.state = {
       colors: [
         'red',
@@ -36,8 +42,23 @@ export default class App extends Component {
 
     setInterval(() => {
 
+      /**
+       * Functional programming
+       * immutability & statelessness
+       */
       this.setState(previousState => {
 
+        /**
+         * The slice() method extracts a section of a string and returns it as a new string, without modifying the original string.
+         *
+         * beginIndex (first arg): The zero-based index at which to begin extraction. If negative, it is treated as strLength + beginIndex
+         * where strLength is the length of the string (for example, if beginIndex is -3 it is treated as strLength - 3). If beginIndex
+         * is greater than or equal to the length of the string, slice() returns an empty string.
+         *
+         * endIndex (second arg): Optional. The zero-based index before which to end extraction. The character at this index will not be
+         * included. If endIndex is omitted, slice() extracts to the end of the string. If negative, it is treated as strLength + endIndex
+         * where strLength is the length of the string (for example, if endIndex is -3 it is treated as strLength - 3).
+         */
         let lastColor        = previousState.colors.slice(-1);
         let withoutLastColor = previousState.colors.slice(0, -1);
 
@@ -56,26 +77,28 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     flex: 1
   },
+
+  // Seven colors (rainbow)
   redBg: {
-    backgroundColor: 'rgb(255,  0,  0)'
+    backgroundColor: 'rgba(255,  0,  0, .5)'
   },
   orangeBg: {
-    backgroundColor: 'rgb(255,127,  0)'
+    backgroundColor: 'rgba(255,127,  0, .5)'
   },
   yellowBg: {
-    backgroundColor: 'rgb(255,255,  0)'
+    backgroundColor: 'rgba(255,255,  0, .5)'
   },
   greenBg: {
-    backgroundColor: 'rgb(  0,255,  0)'
+    backgroundColor: 'rgba(  0,255,  0, .5)'
   },
   blueBg: {
-    backgroundColor: 'rgb(  0,  0,255)'
+    backgroundColor: 'rgba(  0,  0,255, .5)'
   },
   indigoBg: {
-    backgroundColor: 'rgb( 75,  0,130)'
+    backgroundColor: 'rgba( 75,  0,130, .5)'
   },
   violetBg: {
-    backgroundColor: 'rgb(143,  0,255)'
+    backgroundColor: 'rgba(143,  0,255, .5)'
   }
 
 });
